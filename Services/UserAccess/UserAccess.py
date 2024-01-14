9c4ccf574fb885e9ba9ecfac115803ff25185bd2
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2024-01-14 00:55:41 +0000

    User Access Service Update

---
+++ Services/UserAccess/UserAccess.py
@@ -0,0 +1,77 @@
+import json
+import logger
+import os
+
+__log = logger.Logger("UserAccessProvider", True)
+
+ACCESSES_KEYS = {
+    "start": "start",
+    "stop": "stop",
+    "restart": "restart",
+    "commands": "commands",
+    "backup": "backup",
+    "invite": "invite",
+    "uploadMods": "uploadMods",
+    "uploadFiles": "uploadFiles",
+    "downloadMods": "downloadMods",
+    "downloadFiles": "downloadFiles",
+}
+
+def checkInKeys(key):
+    if key in ACCESSES_KEYS.keys():
+        return True
+    else:
+        return False
+    
+def generateAccesses(username):
+    with open(os.path.join("Services", "users.json"), "r") as f:
+        data = json.load(f)
+        if username in data.keys():
+            for i in ACCESSES_KEYS.keys():
+                if i not in data[username]["access"].keys():
+                    data[username]["access"][i] = False
+        else:
+            data[username] = {
+                "access": {}
+            }
+            for i in ACCESSES_KEYS.keys():
+                data[username]["access"][i] = False
+    
+def getAccess(username, key):
+    if checkInKeys(key):
+        with open(os.path.join("Services", "users.json"), "r") as f:
+            data = json.load(f)
+            if username in data.keys():
+                if key in data[username]["access"].keys():
+                    return data[username]["access"][key]
+                else:
+                    return False
+            else:
+                return False
+    else:
+        __log.printerror(f"An error occured while reading users accesses. Key {key} is not in the key list.")
+        return False
+    
+def getFullAccess(username):
+    returnValue = {}
+    for i in ACCESSES_KEYS.keys():
+        returnValue[i] = getAccess(username, i)
+    return returnValue
+
+def setAccess(username, key, value):
+    if checkInKeys(key):
+        with open(os.path.join("Services", "users.json"), "r") as f:
+            data = json.load(f)
+            if username in data.keys():
+                data[username]["access"][key] = value
+            else:
+                data[username] = {
+                    "access": {
+                        key: value
+                    }
+                }
+        with open(os.path.join("Services", "users.json"), "w") as f:
+            json.dump(data, f, indent=4)
+    else:
+        __log.printerror(f"An error occured while reading users accesses. Key {key} is not in the key list.")
+        return False
